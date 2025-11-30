dcbf4311ec5bd6fd27dc0722d412f0b3534f575b
SlimyChip79 <janis.logins11@gmail.com>
2025-11-30 18:38:53 +0200

    Update button_led.py
    file before was fualty

--- button_led.py
+++ button_led.py
@@ -1,36 +1,59 @@
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
 
-# --- GPIO setup ---
-BUTTON_PIN = 5      # Button GPIO
-LED1_PIN = 17       # LED1 GPIO
-LED2_PIN = 27       # LED2 GPIO
+# ----- Pins -----
+BUTTON_PIN = 4
+LED1_PIN = 17
+LED2_PIN = 27
 
+# ----- Timing -----
+debounce_delay = 0.03  # 30 ms
+long_press_threshold = 0.4  # 400 ms
+
+# ----- State -----
+last_button_reading = GPIO.LOW
+last_debounce_time = 0
+button_press_start_time = 0
+long_press_triggered = False
+led1_state = GPIO.LOW
+led2_state = GPIO.LOW
+
+# ----- Setup -----
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED1_PIN, GPIO.OUT)
 GPIO.setup(LED2_PIN, GPIO.OUT)
 
-def led_control(duration):
-    """Light LEDs based on button press duration"""
-    if duration < 2:
-        GPIO.output(LED1_PIN, GPIO.HIGH)
-        GPIO.output(LED2_PIN, GPIO.LOW)
-    else:
-        GPIO.output(LED1_PIN, GPIO.LOW)
-        GPIO.output(LED2_PIN, GPIO.HIGH)
-
+# ----- Main Loop -----
 try:
-    button_pressed_time = None
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            if button_pressed_time is None:
-                button_pressed_time = time.time()
-        else:
-            if button_pressed_time is not None:
-                press_duration = time.time() - button_pressed_time
-                led_control(press_duration)
-                button_pressed_time = None
-        time.sleep(0.05)  # small delay to debounce
+        reading = GPIO.input(BUTTON_PIN)
+        current_time = time.time()
+
+        if reading != last_button_reading:
+            last_debounce_time = current_time
+
+        if (current_time - last_debounce_time) > debounce_delay:
+            if reading == GPIO.HIGH:
+                if button_press_start_time == 0:
+                    button_press_start_time = current_time
+                    long_press_triggered = False
+                elif not long_press_triggered and (current_time - button_press_start_time >= long_press_threshold):
+                    # Long press: toggle LED1
+                    led1_state = not led1_state
+                    GPIO.output(LED1_PIN, led1_state)
+                    long_press_triggered = True
+            else:
+                if button_press_start_time != 0 and not long_press_triggered:
+                    # Short press: toggle LED2
+                    led2_state = not led2_state
+                    GPIO.output(LED2_PIN, led2_state)
+                button_press_start_time = 0
+                long_press_triggered = False
+
+        last_button_reading = reading
+        time.sleep(0.01)
+
 except KeyboardInterrupt:
     GPIO.cleanup()
