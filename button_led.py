06fc2c152ad2117a314a0d30c0c18f1795fa33b1
SlimyChip79 <janis.logins11@gmail.com>
2025-11-30 18:36:30 +0200

    Update button_led.py
    after test worked adding back original

--- button_led.py
+++ button_led.py
@@ -1,15 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 17
+# --- GPIO setup ---
+BUTTON_PIN = 5      # Button GPIO
+LED1_PIN = 17       # LED1 GPIO
+LED2_PIN = 27       # LED2 GPIO
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LED1_PIN, GPIO.OUT)
+GPIO.setup(LED2_PIN, GPIO.OUT)
+
+def led_control(duration):
+    """Light LEDs based on button press duration"""
+    if duration < 2:
+        GPIO.output(LED1_PIN, GPIO.HIGH)
+        GPIO.output(LED2_PIN, GPIO.LOW)
+    else:
+        GPIO.output(LED1_PIN, GPIO.LOW)
+        GPIO.output(LED2_PIN, GPIO.HIGH)
 
 try:
+    button_pressed_time = None
     while True:
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(1)
+        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+            if button_pressed_time is None:
+                button_pressed_time = time.time()
+        else:
+            if button_pressed_time is not None:
+                press_duration = time.time() - button_pressed_time
+                led_control(press_duration)
+                button_pressed_time = None
+        time.sleep(0.05)  # small delay to debounce
 except KeyboardInterrupt:
     GPIO.cleanup()
