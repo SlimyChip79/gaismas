80c8be24179dd654bf5dfe5bcfb0bd2b98d9e619
SlimyChip79 <janis.logins11@gmail.com>
2025-12-21 13:31:19 +0200

    Update gaismas.py
    fix

--- gaismas.py
+++ gaismas.py
@@ -3,11 +3,13 @@ import board
 import busio
 from adafruit_mcp230xx import mcp23017
 from adafruit_pcf8575 import PCF8575
+from adafruit_mcp230xx.digital_inout import DigitalInOut
 
 # -------------------- I2C SETUP --------------------
 i2c = busio.I2C(board.SCL, board.SDA)
+time.sleep(1)  # small delay for bus stability
 
-# MCP boards for inputs
+# MCP23017 boards (two chips)
 mcp1 = mcp23017.MCP23017(i2c, address=0x20)
 mcp2 = mcp23017.MCP23017(i2c, address=0x21)
 
@@ -16,68 +18,57 @@ pcf1 = PCF8575(i2c, address=0x26)
 pcf2 = PCF8575(i2c, address=0x27)
 
 # -------------------- CONFIG --------------------
-# Virtual pins 0-31
-normal_virtual_pins = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]   # 15 normal
-normal_relays_pins  = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-
-special_virtual_pins = [15,16,17,18,19,20,21,22,23]  # 9 special
-special_click_relays = [15,16,17,18,19,20,21,22,23]  # short press
-special_hold_relays  = [24,25,26,27,28,29,30,31,32]  # long press
-
-DEBOUNCE_DELAY = 50        # ms
+DEBOUNCE_DELAY = 50          # ms
 LONG_PRESS_THRESHOLD = 2000  # ms
 
-# -------------------- SPLIT INPUTS MCP1/MCP2 --------------------
-inputs1_pins = []
-inputs2_pins = []
+# -------------------- NORMAL INPUTS (15) --------------------
+# Map 0-31 IDs to MCP pins: (chip, pin)
+normal_inputs_map = [
+    (mcp1, 0), (mcp1, 1), (mcp1, 2), (mcp1, 3), (mcp1, 4),
+    (mcp1, 5), (mcp1, 6), (mcp1, 7), (mcp1, 8), (mcp1, 9),
+    (mcp1, 10), (mcp1, 11), (mcp1, 12), (mcp1, 13), (mcp1, 14)
+]
+normal_relays = list(range(15))  # 0–14 relays
 
-for vp in normal_virtual_pins + special_virtual_pins:
-    if 0 <= vp <= 15:
-        inputs1_pins.append(vp)
-    elif 16 <= vp <= 31:
-        inputs2_pins.append(vp - 16)
-    else:
-        raise ValueError("Virtual pin out of 0-31 range")
+# -------------------- SPECIAL CLICK/HOLD INPUTS (9) --------------------
+special_inputs_map = [
+    (mcp1, 15),  # input 15
+    (mcp2, 0),   # input 16
+    (mcp2, 1),   # input 17
+    (mcp2, 2),   # input 18
+    (mcp2, 3),   # input 19
+    (mcp2, 4),   # input 20
+    (mcp2, 5),   # input 21
+    (mcp2, 6),   # input 22
+    (mcp2, 7)    # input 23
+]
+special_click_relays = list(range(15, 24))  # relays for click
+special_hold_relays  = list(range(24, 33))  # relays for hold
 
 # -------------------- INIT INPUTS --------------------
-inputs1 = [mcp1.get_pin(pin) for pin in inputs1_pins]
-inputs2 = [mcp2.get_pin(pin) for pin in inputs2_pins]
+normal_inputs = [DigitalInOut(chip, pin) for chip, pin in normal_inputs_map]
+for p in normal_inputs:
+    p.switch_to_input()
 
-for p in inputs1 + inputs2:
-    p.switch_to_input()  # pulled-down
+special_inputs = [DigitalInOut(chip, pin) for chip, pin in special_inputs_map]
+for p in special_inputs:
+    p.switch_to_input()
 
-# Special inputs mapped manually to arrays (click/hold)
-special_inputs = []
-for vp in special_virtual_pins:
-    if vp <= 15:
-        special_inputs.append(mcp1.get_pin(vp))
-    else:
-        special_inputs.append(mcp2.get_pin(vp - 16))
-    special_inputs[-1].switch_to_input()
-
-# -------------------- RELAY SAFETY --------------------
+# Safety: all relays OFF (active-low)
 pcf1.write_gpio(0xFFFF)
 pcf2.write_gpio(0xFFFF)
 time.sleep(0.2)
 
 # -------------------- STATE --------------------
-normal_last_values = [0]*len(normal_virtual_pins)
-normal_states = [False]*len(normal_virtual_pins)
+normal_last_values = [0]*len(normal_inputs)
+normal_states = [False]*len(normal_inputs)
 
-special_last = [0]*len(special_virtual_pins)
-special_last_debounce = [0]*len(special_virtual_pins)
-special_press_start = [0]*len(special_virtual_pins)
-special_long_triggered = [False]*len(special_virtual_pins)
+special_last = [0]*len(special_inputs)
+special_last_debounce = [0]*len(special_inputs)
+special_press_start = [0]*len(special_inputs)
+special_long_triggered = [False]*len(special_inputs)
 
-print("15-normal + 9-click/hold controller started (MCP1/MCP2 split)")
-
-# -------------------- HELPER --------------------
-def set_relay_pin(out1, out2, relay_pin):
-    if relay_pin < 16:
-        out1 &= ~(1 << relay_pin)
-    else:
-        out2 &= ~(1 << (relay_pin - 16))
-    return out1, out2
+print("32-input (15 normal + 9 click/hold) controller started")
 
 # -------------------- LOOP --------------------
 while True:
@@ -85,16 +76,19 @@ while True:
     out1 = 0xFFFF
     out2 = 0xFFFF
 
-    # ----- NORMAL TOGGLE -----
-    all_normal_inputs = inputs1 + inputs2
-    for i, pin_obj in enumerate(all_normal_inputs):
+    # ----- NORMAL 15 TOGGLE -----
+    for i, pin_obj in enumerate(normal_inputs):
         val = pin_obj.value
         if val and not normal_last_values[i]:
             normal_states[i] = not normal_states[i]
         normal_last_values[i] = val
 
+        relay_pin = normal_relays[i]
         if normal_states[i]:
-            out1, out2 = set_relay_pin(out1, out2, normal_relays_pins[i])
+            if relay_pin < 16:
+                out1 &= ~(1 << relay_pin)
+            else:
+                out2 &= ~(1 << (relay_pin - 16))
 
     # ----- SPECIAL CLICK/HOLD -----
     for i, pin_obj in enumerate(special_inputs):
@@ -108,11 +102,19 @@ while True:
                     special_press_start[i] = current_time
                     special_long_triggered[i] = False
                 elif not special_long_triggered[i] and (current_time - special_press_start[i] >= LONG_PRESS_THRESHOLD):
-                    out1, out2 = set_relay_pin(out1, out2, special_hold_relays[i])
+                    relay_pin = special_hold_relays[i]
+                    if relay_pin < 16:
+                        out1 &= ~(1 << relay_pin)
+                    else:
+                        out2 &= ~(1 << (relay_pin - 16))
                     special_long_triggered[i] = True
             else:
                 if special_press_start[i] != 0 and not special_long_triggered[i]:
-                    out1, out2 = set_relay_pin(out1, out2, special_click_relays[i])
+                    relay_pin = special_click_relays[i]
+                    if relay_pin < 16:
+                        out1 &= ~(1 << relay_pin)
+                    else:
+                        out2 &= ~(1 << (relay_pin - 16))
                 special_press_start[i] = 0
                 special_long_triggered[i] = False
 
@@ -123,10 +125,9 @@ while True:
     pcf2.write_gpio(out2)
 
     # ----- DEBUG -----
-    print("\nInputs1:", [p.value for p in inputs1])
-    print("Inputs2:", [p.value for p in inputs2])
-    print("Special Inputs:", [p.value for p in special_inputs])
+    print("\nNormal Inputs:", [p.value for p in normal_inputs])
     print("Relays out1: {:016b}".format(out1))
     print("Relays out2: {:016b}".format(out2))
+    print("Special Inputs:", [p.value for p in special_inputs])
 
     time.sleep(0.05)
