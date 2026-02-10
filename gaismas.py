cefe8cd2f7d752bcf560333bf468f86f66532eb7
SlimyChip79 <janis.logins11@gmail.com>
2026-02-10 21:14:03 +0200

    Update gaismas.py
    fix hopefuly

--- gaismas.py
+++ gaismas.py
@@ -3,25 +3,22 @@ import board
 import busio
 from adafruit_pcf8575 import PCF8575
 
-# -------------------- I2C --------------------
 i2c = busio.I2C(board.SCL, board.SDA)
 time.sleep(1)
 
-# -------------------- PCF8575 setup --------------------
 pcf = PCF8575(i2c, address=0x27)
 
-# Start with all relays OFF (active-low -> HIGH = off)
+# All relays OFF initially (active-low)
 pcf.value = 0xFFFF
 
-# -------------------- Blink relays one by one --------------------
 while True:
-    for i in range(8):  # pins 0–7
+    for i in range(8):
         mask = 1 << i
 
-        # Turn relay ON (active-low)
+        # Turn ON
         pcf.value &= ~mask
         time.sleep(0.5)
 
-        # Turn relay OFF
+        # Turn OFF
         pcf.value |= mask
         time.sleep(0.5)
