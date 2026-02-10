e00077c4b334492eb7d2a7fe6114a1bdd8094034
SlimyChip79 <janis.logins11@gmail.com>
2026-02-10 21:07:09 +0200

    Update gaismas.py
    bruv

--- gaismas.py
+++ gaismas.py
@@ -1,18 +1,20 @@
 from adafruit_pcf8574 import PCF8574
-import board
 import busio
+import board
 import time
 
+# Setup I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 pcf = PCF8574(i2c, address=0x27)
 
 # Set all pins as output
-for pin in range(8):
-    pcf.pin(pin).direction = PCF8574.OUTPUT
+pins = [pcf.pin(i) for i in range(8)]
+for p in pins:
+    p.direction = True  # True = OUTPUT
 
-# Turn on/off pins one by one
+# Turn relays on/off one by one
 while True:
-    for pin in range(8):
-        pcf.pin(pin).value = True   # ON
+    for p in pins:
+        p.value = True    # ON
         time.sleep(0.5)
-        pcf.pin(pin).value = False  # OFF
+        p.value = False   # OFF
