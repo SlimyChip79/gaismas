fd2de9779f32322e4dd17e4b1370398a69808bcd
SlimyChip79 <janis.logins11@gmail.com>
2026-02-18 17:51:31 +0200

    Update gaismas.py
    fix hopefuly

--- gaismas.py
+++ gaismas.py
@@ -1,116 +1,120 @@
 #!/usr/bin/env python3
 import time
 from smbus2 import SMBus
-import RPi.GPIO as GPIO
 import board
 import busio
+import RPi.GPIO as GPIO
 from adafruit_pcf8575 import PCF8575
+from datetime import datetime
+
+# ---------------- LOG ----------------
+def log(msg):
+    ts = datetime.now().strftime("%H:%M:%S")
+    print(f"[GAISMAS] {ts} | {msg}", flush=True)
 
 # ---------------- CONFIG ----------------
 I2C_BUS = 1
 
+# PCA extenders
 PCA1_ADDR = 0x20
 PCA2_ADDR = 0x22
-INT1_PIN = 17  # INT for PCA1
-INT2_PIN = 27  # INT for PCA2
+INT1_PIN = 17  # PCA1 INT pin
+INT2_PIN = 27  # PCA2 INT pin
 
+# PCF relays (unchanged)
 PCF1_ADDR = 0x26
 PCF2_ADDR = 0x27
 
-REG_INPUT_0 = 0x00
-REG_INPUT_1 = 0x01
+REG_INPUT_0  = 0x00
+REG_INPUT_1  = 0x01
 REG_OUTPUT_1 = 0x03
 REG_CONFIG_0 = 0x06
 REG_CONFIG_1 = 0x07
 
+POLL_INTERVAL = 0.02  # 20ms
+
 # ---------------- INIT ----------------
-print("[GAISMAS] Initializing...")
+log("Initializing...")
 
-# --- I2C for PCA ---
-bus = SMBus(I2C_BUS)
-
-# --- I2C for PCF ---
-i2c = busio.I2C(board.SCL, board.SDA)
-time.sleep(1)
-
-pcf1 = PCF8575(i2c, address=PCF1_ADDR)
-pcf2 = PCF8575(i2c, address=PCF2_ADDR)
-
-# All relays OFF (active-low)
-pcf1_state = 0xFFFF
-pcf2_state = 0xFFFF
-pcf1.write_gpio(pcf1_state)
-pcf2.write_gpio(pcf2_state)
-
-# Track last input state
-last_input_1 = [1]*16
-last_input_2 = [1]*16
-
-# --- GPIO for PCA interrupts ---
+# GPIO setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(INT1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(INT2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# ---------------- PCA SETUP ----------------
-def setup_pca(addr):
-    """Set P0 inputs, P1 outputs, INT active on input change"""
-    try:
-        # P0 inputs, P1 outputs
-        bus.write_byte_data(addr, REG_CONFIG_0, 0xFF)
-        bus.write_byte_data(addr, REG_CONFIG_1, 0x00)
-        bus.write_byte_data(addr, REG_OUTPUT_1, 0x00)
-        print(f"[GAISMAS] PCA 0x{addr:02X} configured for interrupt mode")
-    except Exception as e:
-        print(f"[GAISMAS] PCA 0x{addr:02X} setup error: {e}")
+# I2C bus
+bus = SMBus(I2C_BUS)
+i2c = busio.I2C(board.SCL, board.SDA)
+time.sleep(1)
 
-setup_pca(PCA1_ADDR)
-setup_pca(PCA2_ADDR)
+# PCF relays
+pcf1 = PCF8575(i2c, address=PCF1_ADDR)
+pcf2 = PCF8575(i2c, address=PCF2_ADDR)
+pcf1_state = 0xFFFF  # all OFF
+pcf2_state = 0xFFFF
+pcf1.write_gpio(pcf1_state)
+pcf2.write_gpio(pcf2_state)
 
-# ---------------- HELPER ----------------
+# PCA last INT pin state (for edge detection)
+last_int1_state = GPIO.input(INT1_PIN)
+last_int2_state = GPIO.input(INT2_PIN)
+
+# Track last PCA input states
+last_input_1 = [1]*16
+last_input_2 = [1]*16
+
+log("Ready (PCA interrupt mode)")
+
+# ---------------- HELPERS ----------------
 def read_pca_inputs(addr):
     p0 = bus.read_byte_data(addr, REG_INPUT_0)
     p1 = bus.read_byte_data(addr, REG_INPUT_1)
     return [(p0 >> i) & 1 for i in range(8)] + [(p1 >> i) & 1 for i in range(8)]
 
-def handle_pca(addr, last_input, pcf, pcf_state, label):
-    """Read PCA input and toggle relays"""
+def handle_pca(addr, last_input, pcf, pcf_state, board_name):
+    """Read PCA inputs, toggle relays if edge detected"""
     inputs = read_pca_inputs(addr)
     for i, val in enumerate(inputs):
         if val == 0 and last_input[i] == 1:
             mask = 1 << i
             if pcf_state & mask:
                 pcf_state &= ~mask
-                print(f"[{label}] Relay {i+1} ON")
+                log(f"[{board_name}] Relay {i+1} ON")
             else:
                 pcf_state |= mask
-                print(f"[{label}] Relay {i+1} OFF")
+                log(f"[{board_name}] Relay {i+1} OFF")
             pcf.write_gpio(pcf_state)
         last_input[i] = val
     return pcf_state
 
-# ---------------- CALLBACKS ----------------
-def pca1_callback(channel):
-    global pcf1_state
-    pcf1_state = handle_pca(PCA1_ADDR, last_input_1, pcf1, pcf1_state, 26)
-
-def pca2_callback(channel):
-    global pcf2_state
-    pcf2_state = handle_pca(PCA2_ADDR, last_input_2, pcf2, pcf2_state, 27)
-
-# Attach interrupt callbacks (falling edge = active LOW)
-GPIO.add_event_detect(INT1_PIN, GPIO.FALLING, callback=pca1_callback, bouncetime=10)
-GPIO.add_event_detect(INT2_PIN, GPIO.FALLING, callback=pca2_callback, bouncetime=10)
-
-print("[GAISMAS] Ready (PCA interrupt mode)")
-
 # ---------------- MAIN LOOP ----------------
 try:
     while True:
-        time.sleep(1)  # main loop just sleeps, all handled via PCA interrupts
+        # -------- POLL PCA1 --------
+        current_int1 = GPIO.input(INT1_PIN)
+        if current_int1 != last_int1_state:
+            last_int1_state = current_int1
+            if current_int1 == GPIO.LOW:  # INT active
+                pcf1_state = handle_pca(PCA1_ADDR, last_input_1, pcf1, pcf1_state, 26)
+
+        # -------- POLL PCA2 --------
+        current_int2 = GPIO.input(INT2_PIN)
+        if current_int2 != last_int2_state:
+            last_int2_state = current_int2
+            if current_int2 == GPIO.LOW:  # INT active
+                pcf2_state = handle_pca(PCA2_ADDR, last_input_2, pcf2, pcf2_state, 27)
+
+        time.sleep(POLL_INTERVAL)
 
 except KeyboardInterrupt:
-    print("Stopping, turning all relays OFF")
+    log("Stopping (KeyboardInterrupt), turning all relays OFF")
+    pcf1.write_gpio(0xFFFF)
+    pcf2.write_gpio(0xFFFF)
+    bus.close()
+    GPIO.cleanup()
+
+except Exception as e:
+    log(f"Fatal error: {e}")
     pcf1.write_gpio(0xFFFF)
     pcf2.write_gpio(0xFFFF)
     bus.close()
